20fca1eb995a249ad6b9a1754625c2802fc32fe3
Amen <amen@arifpay.net>
2023-06-19 10:29:31 +0300

    added qr generator

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,6 +35,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const os_1 = __importDefault(require("os"));
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
@@ -20,6 +44,8 @@ const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const client_1 = require("@prisma/client");
 const date_fns_1 = require("date-fns");
+const fs = __importStar(require("fs"));
+const qrcode = __importStar(require("qrcode"));
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
 const prisma = new client_1.PrismaClient();
@@ -41,6 +67,33 @@ const storage = multer_1.default.diskStorage({
 });
 const upload = (0, multer_1.default)({ storage });
 app.use(express_1.default.static(publicPath));
+function getHostIpAddress() {
+    const interfaces = os_1.default.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        const networkInterface = interfaces[interfaceName];
+        const wlanInterface = networkInterface.find((network) => network.family === "IPv4" &&
+            !network.internal &&
+            interfaceName.toLowerCase().includes("wi-fi"));
+        if (wlanInterface) {
+            return wlanInterface.address;
+        }
+    }
+    return null;
+}
+function generateQRCode(text, filePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const qrCodeBuffer = yield qrcode.toBuffer(text);
+            const fileName = "qrcode.png";
+            const qrCodePath = path_1.default.join(filePath, fileName);
+            fs.writeFileSync(qrCodePath, qrCodeBuffer);
+            return fileName;
+        }
+        catch (error) {
+            throw new Error(`Failed to generate QR code: ${error}`);
+        }
+    });
+}
 app.post("/upload", upload.single("photo"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const file = req.file;
     if (!file) {
@@ -56,9 +109,42 @@ app.post("/upload", upload.single("photo"), (req, res) => __awaiter(void 0, void
     });
     res.send({ url: fileUrl });
 }));
-app.get("/", (req, res) => {
+app.get("/start", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("duu");
+    const hostIp = yield getHostIpAddress();
+    const savePath = path_1.default.join(__dirname, "public/uploads");
+    if (!hostIp) {
+        return res.status(400).json({ error: "Unable to determine host IP" });
+    }
+    yield generateQRCode(hostIp, savePath);
     res.sendFile(path_1.default.join(publicPath, "index.html"));
-});
+}));
+app.post("/generate-qrcode", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const hostIp = yield getHostIpAddress();
+        const savePath = path_1.default.join(__dirname, "public/uploads");
+        if (!hostIp) {
+            return res.status(400).json({ error: "Unable to determine host IP" });
+        }
+        const filename = yield generateQRCode(hostIp, savePath);
+        const url = `http://${hostIp}:3000/uploads/${filename}`;
+        return res.json({ url });
+    }
+    catch (error) {
+        console.log("ERROR: ", error);
+        return res.status(500).json({ error: "Failed to generate QR code" });
+    }
+}));
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log("duu");
+    const hostIp = yield getHostIpAddress();
+    const savePath = path_1.default.join(__dirname, "public/uploads");
+    if (!hostIp) {
+        return res.status(400).json({ error: "Unable to determine host IP" });
+    }
+    yield generateQRCode(hostIp, savePath);
+    res.sendFile(path_1.default.join(publicPath, "index.html"));
+}));
 app.get("/photos", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const today = new Date();
     const startOfToday = (0, date_fns_1.startOfDay)(today);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import os from "os";
 import express, { json } from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -6,6 +7,8 @@ import path from "path";
 import cors from "cors";
 import { PrismaClient, SortOrder } from "@prisma/client";
 import { startOfDay, endOfDay } from "date-fns";
+import * as fs from "fs";
+import * as qrcode from "qrcode";
 
 const app = express();
 const server = http.createServer(app);
@@ -37,6 +40,47 @@ const upload = multer({ storage });
 // Use the public directory to serve static files
 app.use(express.static(publicPath));
 
+function getHostIpAddress() {
+  const interfaces = os.networkInterfaces();
+
+  for (const interfaceName in interfaces) {
+    const networkInterface: any = interfaces[interfaceName];
+
+    // Filter by interface type (e.g., 'WLAN', 'Wi-Fi')
+    const wlanInterface = networkInterface.find(
+      (network: any) =>
+        network.family === "IPv4" &&
+        !network.internal &&
+        interfaceName.toLowerCase().includes("wi-fi")
+    );
+
+    if (wlanInterface) {
+      return wlanInterface.address;
+    }
+  }
+
+  return null;
+}
+
+// generate QR-Code
+async function generateQRCode(text: string, filePath: string): Promise<string> {
+  try {
+    // Generate QR code
+    const qrCodeBuffer = await qrcode.toBuffer(text);
+
+    // Save QR code to file
+    const fileName = "qrcode.png";
+    const qrCodePath = path.join(filePath, fileName);
+    fs.writeFileSync(qrCodePath, qrCodeBuffer);
+
+    // Return URL of the saved QR code
+    // const url = `http://example.com/${fileName}`;
+    return fileName;
+  } catch (error) {
+    throw new Error(`Failed to generate QR code: ${error}`);
+  }
+}
+
 app.post("/upload", upload.single("photo"), async (req, res) => {
   const file = req.file;
   if (!file) {
@@ -53,9 +97,50 @@ app.post("/upload", upload.single("photo"), async (req, res) => {
   });
   res.send({ url: fileUrl });
 });
+app.get("/start", async (req, res) => {
+  console.log("duu");
+  const hostIp = await getHostIpAddress(); // Extract the client's IP address from the request
+  const savePath = path.join(__dirname, "public/uploads");
 
+  if (!hostIp) {
+    return res.status(400).json({ error: "Unable to determine host IP" });
+  }
+
+  await generateQRCode(hostIp, savePath);
+
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.post("/generate-qrcode", async (req, res) => {
+  try {
+    const hostIp = await getHostIpAddress(); // Extract the client's IP address from the request
+    const savePath = path.join(__dirname, "public/uploads");
+
+    if (!hostIp) {
+      return res.status(400).json({ error: "Unable to determine host IP" });
+    }
+
+    const filename = await generateQRCode(hostIp, savePath);
+    const url = `http://${hostIp}:3000/uploads/${filename}`;
+
+    return res.json({ url });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    return res.status(500).json({ error: "Failed to generate QR code" });
+  }
+});
 // Serve index.html as the root page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  console.log("duu");
+  const hostIp = await getHostIpAddress(); // Extract the client's IP address from the request
+  const savePath = path.join(__dirname, "public/uploads");
+
+  if (!hostIp) {
+    return res.status(400).json({ error: "Unable to determine host IP" });
+  }
+
+  await generateQRCode(hostIp, savePath);
+
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
